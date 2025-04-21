1610d4937915f065b959a21734c27972b82f9752
Alexey Jukov <nitroalexus@gmail.com>
2025-04-21 18:38:45 +0600

    Arrow direction == wind direction

---
+++ Assets/SailMovingProject/Scripts/ArrowPointer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ArrowPointer : MonoBehaviour
+{
+    [SerializeField] private Wind _wind;
+    [SerializeField] private float _rotationSpeed = 2f;
+
+    void LateUpdate()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(_wind.Direction.normalized, Vector3.up);
+        
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            _rotationSpeed * Time.deltaTime
+        );
+    }
+}
---
+++ Assets/SailMovingProject/Scripts/Wind.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] private float _minSpeed = 5;
+    [SerializeField] private float _maxSpeed = 10;
+    [SerializeField] private float _changeDirectionTime = 5;
+
+    private float _minRotationAngle = 10;
+    private float _maxRotationAngle = 350;
+    private float _timer = 0;
+
+    public float Speed { get; private set; }
+    public Vector3 Direction { get; private set; }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer > _changeDirectionTime)
+        {
+            _timer = 0;
+            Direction = GetRandomDirectionXZ();
+        }
+    }
+
+    private Vector3 GetRandomDirectionXZ()
+    {
+        Vector3 direction = Vector3.forward;
+
+        float randomAngle = Random.Range(_minRotationAngle, _maxRotationAngle);
+        Quaternion turn = Quaternion.Euler(0, randomAngle, 0);
+
+        return turn * direction;
+    }
+}
